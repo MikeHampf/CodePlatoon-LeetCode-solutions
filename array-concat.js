350fed4dfb66561530d35ae30617e5bf78616e2f
MikeHampf <hampfma5@gmail.com>
2023-10-24 18:23:52 -0400

    2 new solutions

---
+++ array-concat.js
@@ -0,0 +1,9 @@
+var getConcatenation = function (nums) {
+  let ans = [];
+  for (let j = 0; j < 2; j++) {
+    for (let i = 0; i < nums.length; i++) {
+      ans.push(nums[i]);
+    }
+  }
+  return ans;
+};
